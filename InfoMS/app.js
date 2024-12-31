9b61872619d4b83a4bdbc3af93ae812c54743675
Yusufjenom <jenomy32@gmail.com>
2024-12-31 15:46:04 +0100

    update

---
+++ InfoMS/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => console.log(`server running on port: ${port}`));
