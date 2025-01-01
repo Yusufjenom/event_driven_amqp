0768ed91b3e463c2801bcba37fd62a734f5e4e24
Yusufjenom <jenomy32@gmail.com>
2025-01-01 17:30:52 +0100

    update

---
+++ WarningAndErrorMS/app.js
@@ -0,0 +1,37 @@
+//import express from "express";
+import amqp from "amqplib";
+
+// const app = express();
+// const port = 3000;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+const consumeMessage = async () => {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  await channel.assertExchange("logExchange", "direct");
+
+  const queue = await channel.assertQueue("WarningAndErrorQueue");
+
+  //additional bindings for the warning and eerror queue
+  await channel.bindQueue(queue.queue, "logExchange", "Warning");
+  await channel.bindQueue(queue.queue, "logExchange", "Error");
+
+  await channel.consume(queue.queue, (msg) => {
+    const data = JSON.parse(msg.content);
+    console.log(data);
+    channel.ack(msg);
+  });
+};
+
+// app.get("/test", async (req, res) => {
+//   const data = await consumeMessage();
+//   res.status(200).json({
+//     success: true,
+//     message: data,
+//   });
+// });
+consumeMessage();
+//app.listen(port, () => console.log(`server running on port: ${port}`));
