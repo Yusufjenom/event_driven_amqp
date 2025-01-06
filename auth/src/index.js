845ff9abccce4a7ffd71f9b26955993d4d575691
Yusufjenom <jenomy32@gmail.com>
2025-01-06 13:39:59 +0100

    update

--- auth/src/index.js
+++ auth/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { Config } from "./config/index.js";
 import { ErrorHandlerMiddleware } from "./infrastructure/express/middleware/errorHandlerMiddleware.js";
 import { connectDb } from "./infrastructure/database/index.js";
+import UserRouter from "./infrastructure/express/routes/user.js";
 
 const app = express();
 const port = Config.PORT;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "*" }));
 
+app.use("/api/v1", UserRouter);
 app.use(ErrorHandlerMiddleware);
 
 app.listen(port, async () => {
