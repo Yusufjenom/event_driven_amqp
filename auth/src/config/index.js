69c0eb6a4b559875b94e2a5f3eb834a1427feb5c
Yusufjenom <jenomy32@gmail.com>
2025-01-06 12:42:49 +0100

    update

--- auth/src/config/index.js
+++ auth/src/config/index.js
@@ -4,4 +4,8 @@ dotenv.config();
 export const Config = {
   PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
+  rabbitMQ: {
+    url: "amqp://localhost",
+    exchangeName: "logExchange",
+  },
 };
